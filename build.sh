48fa2dff43663e92c9d45132e990a1e96a32637c
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2026-05-03 14:53:14 +0200

    fix: fixing help string

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ repl()
 
 repl_project_name()
 {
-	local CT_PROJECT_NAME_FILES="CMakeLists.txt .gitignore"
+	local CT_PROJECT_NAME_FILES="CMakeLists.txt .gitignore src/utils/misc.c"
 	local project_name
 	printf "%s" "Enter the project name : "
 	read project_name
--- src/utils/misc.c
+++ src/utils/misc.c
@@ -3,5 +3,5 @@
 void	print_help(void)
 {
 	printf("\
-Usage: ft_nm [OPTIONS] [FILE]\n");
+Usage: {{CT_PROJECT_NAME}} [OPTIONS] [FILE]\n");
 }
